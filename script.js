b6b359f2c95b2f7aa2128a1209ca487921e2743d
Eugeniusz <{ID}+{username}@users.noreply.github.com>
2025-06-10 22:32:07 +0200

    Add changing color on hover

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ for(i = 0; i < gridSize; i++) {
     for (j = 0; j < gridSize; j++) {
         const gridSqr = document.createElement('div');
         gridSqr.classList.add('grid-sqr');
+        gridSqr.addEventListener('mouseover', () => {gridSqr.classList.add('hovered')});
+        gridSqr.addEventListener('mouseout', () => {gridSqr.classList.remove('hovered')});
         gridContainer.appendChild(gridSqr);
     }
 }

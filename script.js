6385910e72cfcfe394bdf68fb412df67460b6416
Eugeniusz <{ID}+{username}@users.noreply.github.com>
2025-06-11 21:03:37 +0200

    Add generate grid popup

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const gridContainer = document.querySelector('.container');
+const popup = document.querySelector('.popup');
+
+const newGridBtn = document.querySelector('.new-grid-btn');
+const popupCloseBtn = document.querySelector('.close-btn');
+const createGridBtn = document.querySelector('.create-btn');
+
+const gridSizeInput = document.querySelector('#grid-size');
 const gridSize = 4;
-for(i = 0; i < gridSize; i++) {
-    for (j = 0; j < gridSize; j++) {
-        const gridSqr = document.createElement('div');
-        gridSqr.classList.add('grid-sqr');
-        gridSqr.addEventListener('mouseover', () => {gridSqr.classList.add('hovered')});
-        gridSqr.addEventListener('mouseout', () => {gridSqr.classList.remove('hovered')});
-        gridContainer.appendChild(gridSqr);
+
+popup.classList.add('hidden');
+newGridBtn.addEventListener('click', () => {
+    popup.classList.remove('hidden');
+});
+createGridBtn.addEventListener('click', () => {
+    popup.classList.add('hidden');
+});
+popupCloseBtn.addEventListener('click', () => {popup.classList.add('hidden');});
+
+generateGrid();
+
+function generateGrid (size = gridSize) {
+    console.log(size);
+    for(i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            const gridSqr = document.createElement('div');
+            gridSqr.classList.add('grid-sqr');
+            gridSqr.addEventListener('mouseover', () => {gridSqr.classList.add('hovered')});
+            gridSqr.addEventListener('mouseout', () => {gridSqr.classList.remove('hovered')});
+            gridContainer.appendChild(gridSqr);
+        }
     }
 }
